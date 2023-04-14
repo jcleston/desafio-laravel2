29ebbb2afd940cfa4ef9acd9af5c5cd6fd9e8418
jcleston <janes.cleston.silva@gmail.com>
2023-04-14 16:59:29 -0300

    feat: add routes noticia in project

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,11 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
+Route::controller(App\Http\Controllers\NoticiaController::class)->group(function () {
+    Route::get('/noticias', 'index')->middleware(['auth', 'verified'])->name('index');
+    Route::get('/cadastrar-noticia', 'create')->middleware(['auth', 'verified'])->name('cadastrar-noticia');
+    Route::post('/cadastrar-noticia', 'store')->middleware(['auth', 'verified'])->name('cadastrar-noticia');
+    Route::get('/editar-noticia/{id_noticia}', 'edit')->middleware(['auth', 'verified'])->name('editar-noticia');
+    Route::put('/editar-noticia/{id_noticia}', 'update')->middleware(['auth', 'verified'])->name('editar-noticia');
+    Route::delete('/deletar-noticia/{id_noticia}', 'destroy')->middleware(['auth', 'verified'])->name('deletar-noticia');
+});
